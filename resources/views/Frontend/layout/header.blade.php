67066f655681bbc0dc8cd8476e8f81563f8031c8
= <bibekaryal0@gmail.com>
2019-10-30 20:03:24 +0545

    ok

--- resources/views/Frontend/layout/header.blade.php
+++ resources/views/Frontend/layout/header.blade.php
@@ -39,7 +39,7 @@
                             </ul>
                         </li>
                         <li><a href="../about.blade.php">Travel News</a></li>
-                        <li><a href="{{route('aobut')}}">About</a></li>
+                        <li><a href="{{route('about')}}">About</a></li>
                         <li><a href="{{route('blogs')}}">Blog</a></li>
 
                         <li><a href="../contact.html">Contact</a></li>
